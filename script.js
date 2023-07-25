5f9fc35487732080a93915910c02af72ec6a7b4c
FransXave <santhothosa@gmail.com>
2023-07-25 11:13:55 -0700

    message

---
+++ script.js
@@ -0,0 +1,9 @@
+//add box shadow
+function addShadow(element) {
+  element.classList.add("shadow");
+}
+
+// remove box shadow
+function removeShadow(element) {
+  element.classList.remove("shadow");
+}
