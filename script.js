23bef6a6071eff4942e5ca168158ab802a61dafc
FransXave <santhothosa@gmail.com>
2023-07-25 23:42:07 -0700

    cookies box added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let cookieObj = document.querySelector(".cookie-policy-box");
+
+function removeMe() {
+  cookieObj.remove();
+}
+
 //add box shadow
 function addShadow(element) {
   element.classList.add("shadow");
